7ce3e85942382350c2395ebabb896c7783ab1ede
YoussefHany3 <101423705+YoussefHany3@users.noreply.github.com>
2022-04-26 01:47:30 +0200

    Create login2.php

---
+++ login2.php
@@ -0,0 +1,9 @@
+<?php
+
+?>
+<form action="dologin2.php" method="post">
+	
+	Email<input type="text" name="Email"/>
+	Password<input type="password" name="Password"/>
+	<input type="submit" />
+</form>
