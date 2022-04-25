2f4a623221b8274c5762190600e2673fab2e878e
ahmedsayed1122 <100765907+ahmedsayed1122@users.noreply.github.com>
2022-04-25 02:13:12 +0200

    user

--- Project oop/user.php
+++ Project oop/user.php
@@ -1,3 +1,16 @@
 <?php
+include "Functins.php";
+$fileName="UsersFile.txt";
 
+function Login ($Email,$Password)
+{
+	global $fileName;
+	if (searchUser($fileName,$Email."~".$Password))
+	{
+		return true;
+	}
+	else{
+		return false;
+	}
+}
 ?>
