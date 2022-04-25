a7cffc3307400bab94142ce72d84ef8886cd7a0e
ahmedsayed1122 <100765907+ahmedsayed1122@users.noreply.github.com>
2022-04-25 21:48:37 +0200

    parent

--- parent.php
+++ parent.php
@@ -1,2 +1,52 @@
 <?php
+class parent extends user 
+{
+    public $students;
+    public $teachert;
+    private $fees;
+    public function Contactwithteachers($id,$email)
+    {
+
+    }
+    public function viewmarks($sid, $id)
+    {
+
+    }
+    public function viewstudentperofrmamnce($sid,$id)
+    {
+
+    }
+    public function viewtimetable($sid,$id)
+    {
+
+    }
+    public function viewtheattendance ($sid,$id )
+    {
+
+    }
+    public function payfees($id,$sschoolyear,$ffees, $fpaytype)
+    {
+        
+    }
+
+
+
+	/**
+	 * 
+	 * @return mixed
+	 */
+	function getFees() {
+		return $this->fees;
+	}
+	
+	/**
+	 * 
+	 * @param mixed $fees 
+	 * @return parent
+	 */
+	function setFees($fees): self {
+		$this->fees = $fees;
+		return $this;
+	}
+}
 ?>
