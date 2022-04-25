0ccc3070dba89ac9d75faef68d8a8e8405829be0
MBahaa24 <mohamed.bahaa4@msa.edu.eg>
2022-04-25 23:42:15 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,2 +1,32 @@
 <?php
+include_once('student.php');
+include_once('teacher.php');
+    class Admin extends user
+    {
+        public $securenum;
+        
+        public $teacherobj=new teacher();
+
+
+        public function modify_teacher($teacherobj.id,$teacherobj.name,$teacherobj.email,$teacherobj.phonenumber)
+        {
+
+        }
+        public function updateusers($id,$name,$email,$phonenumber)
+        {
+
+        }
+        public function deleteusers($id,$password)
+        {
+
+        }
+        public function make_timetable($id,$password)
+        {
+
+        }
+        public function takesalary($id,$password)
+        {
+
+        }
+    }
 ?>
