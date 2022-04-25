2151655a291fdb169d614aecca4d931e339a3f77
ahmedsayed1122 <100765907+ahmedsayed1122@users.noreply.github.com>
2022-04-25 22:16:47 +0200

    student

--- student.php
+++ student.php
@@ -1,3 +1,48 @@
 <?php
+class student extends user
+{
+    public $shcoolyear;
+    public $subject;
+    public $fees;
+    public function  showresults($id,$password)
+    {
 
+    }
+    public function showscedule($id)
+ 
+    {
+        
+    }
+    public function  solveexam($id,$password,$kid )
+    {
+        
+    }
+    public function  viewhomework($kid ,$schoolyear )
+
+    {
+        
+    }
+    public function  submithomework($kid ,$schoolyear )
+    {
+        
+    }
+    public function  viewteacherfeedback( $id,$email)
+    {
+        
+    }
+    public function  registersubject($kid ,$kname,$klevel )
+    {
+        
+    }
+    public function viewthefees($id,$fpayment,$amount) 
+    {
+        
+    }
+    public function  borrowbook($bid)
+    {
+        
+    }
+
+
+}
 ?>
