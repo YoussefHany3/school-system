87a430987b357022b536a8ba9d6dabd27fea5cc0
YoussefHany3 <101423705+YoussefHany3@users.noreply.github.com>
2022-04-26 01:58:20 +0200

    ....

---
+++ RegistirationForm.php
@@ -0,0 +1,26 @@
+<?php
+
+?><form action="RegisterForm2.php" method="post">
+<table>
+	<tr>
+		<td>Email</td>
+		<td><input type="text" name="Email"/></td>
+	</tr>
+	<tr>
+		<td>Password</td>
+		<td><input type="password" name="Password" /></td>
+	</tr>
+	<tr>
+		<td>Full Name</td>
+		<td><input type="text" name="FullName"/></td>
+	</tr>
+	<tr>
+		<td>DOB</td>
+		<td><input type="text" name="DOB" /></td>
+	</tr>
+	<tr>
+		<td><input type="submit" /></td>
+		<td><input type="reset" /></td>
+	</tr>
+</table>
+</form>
