59d2d2428832e96b42ba49b09450240e5083c0ab
waagdyyy <100765887+waagdyyy@users.noreply.github.com>
2022-04-25 21:48:58 +0200

    Update manger.php

--- manger.php
+++ manger.php
@@ -4,10 +4,71 @@ include_once("student.php");
 include_once("teacher.php");
 class manager extends user{
 
-   public $id;
-   public $fileName;
+    public $securemangnum;
+  
+   private function send_reports($id,$name,$Email,$securemangnum)
+   {
 
-   public $Email;
-   private function send reports();
+   }
+   private function remote_parent_meeting($id,$name,$Email)
+   {
+
+   }
+   private function give_salary($id,$salary)
+   {
+
+   }
+   private function view_teacher($id)
+   {
+
+   }
+   private function send_depraviation($id,$Email,$securemangnum)
+   {
+
+   }
+  private function make_promotions($id,$securemangnum)
+  {
+
+  }
+  private function make_learning_resource($securemangnum)
+  {
+
+  }
+  private function upload_admissins_paper($securemangnum)
+  {
+
+  }
+  private function make_reports($securemangnum)
+  {
+
+  }
+  private function make_interview($id,$name,$Email,$phonenumber)
+  {
+
+  }
+  private function take_salary($id,$password)
+  {
+
+  }
+  private function take_staff_attendance($id)
+  {
+
+  }
+  private function give_deductions($name,$date,$salary)
+  {
+
+  }
+  private function add_usaer($id,$Email,$name,$password,$phonenumber)
+  {
+
+  }
+  private function delete_user($id,$password)
+  {
+
+  }
+  private function update($id,$name,$Email,$password,$phonenumber)
+  {
+      
+  }
 }
 ?>
