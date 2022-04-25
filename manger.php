ead18675b7f52180aa0347d6b9ff40ef548acfd2
waagdyyy <100765887+waagdyyy@users.noreply.github.com>
2022-04-25 20:59:57 +0200

    Update manger.php

--- manger.php
+++ manger.php
@@ -1,2 +1,10 @@
 <?php
+include_once("user.php");
+include_once("student.php");
+include_once("teacher.php");
+class manager extends user
+class manager extends student
+{
+private function remoteparentmeetings($id,$name,$Email)
+}
 ?>
