a29c7f6705bc34b155601a847a474d88dca684ab
YoussefHany3 <101423705+YoussefHany3@users.noreply.github.com>
2022-04-25 22:09:03 +0200

    Update accountant.php

--- accountant.php
+++ accountant.php
@@ -1,2 +1,47 @@
 <?php
+include_once "Functins.php";
+include_once "salary";
+
+class accountant extends user{
+private $salary;
+
+public function Login ($Email,$Password)
+{
+	global $fileName;
+	if (searchUser($fileName,$Email."~".$Password))
+	{
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+public function payment($id,$salary){
+
+}
+
+public function calculatesalary($id,$salary){
+
+
+}
+
+public function managestaffinance($id,$salary){
+
+}
+
+public function takesalary($id,$Password){
+
+}
+
+function getSalary() {
+    return $this->salary;
+}
+
+function setSalary($salary): self {
+    $this->salary = $salary;
+    return $this;
+}
+}
+
 ?>
