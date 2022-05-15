eeb946a2c10d2fe1fccc9d57d523f304211ff717
Mahmoudwa2l <104329809+Mahmoudwa2l@users.noreply.github.com>
2022-05-15 08:23:32 +0200

    Add files via upload

---
+++ SMS/Admin/education multi pages website/user.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+<!-- custom css file link  -->
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" >
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  
+    <div class="container my-5">
+        <form method="post">
+            <div class="mb-3">
+                <label>Name</label>
+                <input type="text" class="form-control" placeholder="Enter User Name" name="name">
+            </div>
+            <div class="mb-3">
+                <label>Email</label>
+                <input type="text" class="form-control" placeholder="Enter User Email" name="email">
+            </div>
+            <div class="mb-3">
+                <label>Mobile</label>
+                <input type="number" class="form-control" placeholder="Enter User Mobile" name="mobile">
+            </div>
+            <div class="mb-3">
+                <label>Gender</label>
+                <select class="form-control" placeholder="Enter User Type">
+                    <option value="male">Male</option>
+                    <option value="femal">Female</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label>User type</label>
+                <select class="form-control" placeholder="Enter User Type">
+                    <option value="teacher">Teacher</option>
+                    <option value="admin">Admin</option>
+                    <option value="parent">Parent</option>
+                    <option value="student">Student</option>
+                </select>
+                
+            </div>
+            <div class="mb-3">
+                <label>Date of join</label>
+                <input type="date" class="form-control" placeholder="Enter User DOJ" name="name">
+            </div>
+            <div class="mb-3">
+                <label>Date of Birth</label>
+                <input type="date" class="form-control" placeholder="Enter User DOB" name="name">
+            </div>
+          
+            <div class="mb-3">
+                <label>Password</label>
+                <input type="text" class="form-control" placeholder="Enter User Password" name="name">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <section class="footer">
+
+
+
+
+    </div>
+
+    
+
+    
+
+  </body>
+</html>
