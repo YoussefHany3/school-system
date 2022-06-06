0a2d431392b02366787c321f50b620406e9c66ce
Mahmoudwa2l <104329809+Mahmoudwa2l@users.noreply.github.com>
2022-06-06 17:28:07 +0200

    Add files via upload

---
+++ FINAL PROJECT SMS/FINAL PROJECT/views/teacher/teacherUploadMark.php
@@ -0,0 +1,24 @@
+<?php
+    require("../../controllers/header.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload Mark Sheet</title>
+</head>
+<body>
+
+<?php
+
+echo "This is upload mark sheet page!";
+
+?>
+
+    
+</body>
+</html>
